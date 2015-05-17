1477eda5412c16ac1303375605ce9fd167991ab1
Persio Flexa <flllexa@live.com>
2015-05-17 16:58:32 -0300

    nuget

--- ugapi/Properties/AssemblyInfo.cs
+++ ugapi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IuguApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("UgApi")]
+[assembly: AssemblyDescription("UgApi is a implementation of IUGU payment modules.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IuguApi")]
+[assembly: AssemblyCompany("Flllexa")]
+[assembly: AssemblyProduct("UgApi")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
